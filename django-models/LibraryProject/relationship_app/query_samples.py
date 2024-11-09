56360d2575f313dedf85185d21e0f90b6055427c
Onome <Onome@Onomes-Macbook-Pro.local>
2024-11-09 20:29:36 +0000

    Updated

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
 def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)
+    author = Author.objects.filter(author=author)
     books = author.books.all()
 
     for book in books:
