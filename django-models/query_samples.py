bf4ca8442324ccb63cef99a72700de1ee2433398
Onome <Onome@Onomes-Macbook-Pro.local>
2024-11-09 20:15:52 +0000

    Updated

---
+++ django-models/query_samples.py
@@ -0,0 +1,21 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+
+    for book in books:
+        print(book.title)
+
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+
+    for book in books:
+        print(book.title)
+
+    
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    print(f"The librarian for {library.name} is {librarian.name}")
