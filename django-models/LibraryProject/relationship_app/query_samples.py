c6c61f4ea804157784a65304ad4831104307d2fa
Onome <Onome@Onomes-Macbook-Pro.local>
2024-11-09 22:34:51 +0000

    Updated

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,34 +1,13 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# def books_by_author(author_name):
-#     author_name = Book.objects.filter(author=author)
-#     author = Author.objects.get(name=author_name)
-#     books = author.books.all()
-
-#     for book in books:
-#         print(book.title)
-
-# def books_in_library(library_name):
-#     library = Library.objects.get(name=library_name)
-#     books = library.books.all()
-
-#     for book in books:
-#         print(book.title)
-
-    
-# def librarian_for_library(library_name):
-#     library = Librarian.objects.get(library='')
-#     librarian = library.librarian
-#     print(f"The librarian for {library.name} is {librarian.name}")
-
-
 
 def books_by_author(author_name):
-    author = Author.objects.filter(name=author_name).first()
+    #Get the author name using filter to avoid raising an exception if not found
+    author = Author.objects.filter(name=author_name).first()        #returns None if no match is found
 
     if author:
-
-        books = Book.objects.filter(author=author)
+            #use the reverse relation to get all books for the author
+        books = Book.objects.filter(author=author)      #filter books by the author
 
         for book in books:
             print(book.title)
@@ -36,11 +15,12 @@ def books_by_author(author_name):
         print(f"No author found with {author_name}")
 
 def book_in_library(library_name):
+    #Get the library object using filter to avoid exceptions
     library = Library.objects.filter(name=library_name).first()
 
     if library:
-
-        books = Book.objects.filter(library=library)
+        #filter books associated with the library
+        books = Book.objects.filter(library=library)        #filter books by the library
 
         for book in books:
             print(book.title)
@@ -49,11 +29,11 @@ def book_in_library(library_name):
         print(f"No library found with name {library_name}")
 
 def librarian_for_library(library_name):
-
+    #Get the library object using filter to avoid exceptions
     library = Library.objects.filter(name=library_name).first()
 
     if library:
-
+        #Get the librarian using filter to avoid exceptions
         librarian = Librarian.objects.filter(library=library).first()
 
         if librarian:
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
+
 
 # Create your views here.
+
+def list_books(request):
+    """Retrieves all books renders a list of book titles and their authors."""
+    books = Book.objects.all()      #fetch all book instances from the database
+    context = {'list_books': books} #creates a context dictionary with book list 
+    return render(request, 'books/list_books.html', context)
+
+from django.views.generic import DetailView
+from .models import Library
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'Library_detail.html'
+    context_object_name = 'library'
+
+    
