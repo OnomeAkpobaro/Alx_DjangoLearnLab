72e02baab063e0f1bdc0002fa68f6e832baeaaef
Onome <Onome@onomes-mbp.home>
2024-11-10 22:04:09 +0000

    Updated

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -29,8 +29,13 @@ urlpatterns = [
     path('delete/<int:book_id>/', views.delete_book, name='delete_book'),
 
     path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.libriarian_views, name='liberian_views'),
-    path('member/', views.member_view, name='member_view')
-]
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('books/add_book/', views.add_book, name='add_book'),
+    path('books/edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete_book/<int:pk>/', views.delete_book, name='delete_book'),
+    ]
+
+
 
 
