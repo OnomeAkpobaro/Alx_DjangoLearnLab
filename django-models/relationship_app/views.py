2f7084027388464e6f10730ec055ec25c5be6c22
Onome <Onome@Onomes-Macbook-Pro.local>
2024-11-10 07:37:29 +0000

    Updated

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,8 @@
+from multiprocessing import context
 from django.shortcuts import render
 from .models import Author, Book, Library, Librarian
+from django.views.generic import DetailView
+from .models import Library
 
 
 # Create your views here.
@@ -7,16 +10,15 @@ from .models import Author, Book, Library, Librarian
 def list_books(request):
     """Retrieves all books renders a list of book titles and their authors."""
     books = Book.objects.all()      #fetch all book instances from the database
-    context = {'list_books': books} #creates a context dictionary with book list 
+    context = {'list_books': books} #creates a context dictionary with list of books
     return render(request, "relationship_app/list_books.html", context)
-
-from django.views.generic import DetailView
-from .models import Library
-
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/Library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = 'library'
 
+    def get_context_data(self, **kwargs):
+        return context
+    
     
