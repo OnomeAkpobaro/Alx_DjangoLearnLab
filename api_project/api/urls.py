608aca74569e681b245b91ed2e3ba359b4befb73
Onome <Onome@onomes-mbp.home>
2024-11-22 19:32:37 +0000

    Updated

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),   #Map to the BookList view
+    path('Books/<int:pk>/'),
+    path('', include(router.urls)), #This includes al routes registered with the router
 ]
