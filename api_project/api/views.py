41f10b83af9b018d16f068c29ece1eebef83a4d7
Onome <Onome@onomes-mbp.home>
2024-11-22 19:36:21 +0000

    Updated

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,56 +4,61 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework import viewsets
 from rest_framework.viewsets import ModelViewSet
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, JsonResponse
-from rest_framework.parsers import JSONParser
+# from django.views.decorators.csrf import csrf_exempt
+# from django.http import HttpResponse, JsonResponse
+# from rest_framework.parsers import JSONParser
+
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()       #Fetches all book objects
     serializer_class = BookSerializer   #use the Bookserializer to serialize the queryset
 
-@csrf_exempt
-def BookList(request):
-    """
-    List all Books, or Create a new Book
-    """
-    if request.method == 'GET':
-        Book = Book.objects.all()
-        serializer = BookSerializer(Book, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BookSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.al()        #Queryset to retrieve all books
+    serializer_class =  BookSerializer  #Uses Bookserializer to serialize the data
+# @csrf_exempt
+# def BookList(request):
+#     """
+#     List all Books, or Create a new Book
+#     """
+#     if request.method == 'GET':
+#         Book = Book.objects.all()
+#         serializer = BookSerializer(Book, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = BookSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)
     
-@csrf_exempt
-def BookViewSet(request, pk):
-    """
-    Retrieve, update or delete a book
-    """
-    try:
-        Book = Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        return HttpResponse(status=404)
-    if request.method == 'GET':
-        serializer = BookSerializer(Book)
-        return JsonResponse(serializer.data)
+# @csrf_exempt
+# def BookViewSet(request, pk):
+#     """
+#     Retrieve, update or delete a book
+#     """
+#     try:
+#         Book = Book.objects.get(pk=pk)
+#     except Book.DoesNotExist:
+#         return HttpResponse(status=404)
+#     if request.method == 'GET':
+#         serializer = BookSerializer(Book)
+#         return JsonResponse(serializer.data)
     
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BookSerializer(Book, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-    elif request.method =='DELETE':
-        Book.delete()
-        return HttpResponse(status=204)
+#     elif request.method == 'PUT':
+#         data = JSONParser().parse(request)
+#         serializer = BookSerializer(Book, data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data)
+#         return JsonResponse(serializer.errors, status=400)
+#     elif request.method =='DELETE':
+#         Book.delete()
+#         return HttpResponse(status=204)
     
 
     
