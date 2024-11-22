6743a67d14a717e326c2aaada5ab34f2f119dede
Onome <Onome@onomes-mbp.home>
2024-11-22 18:05:33 +0000

    Updated

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+# Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()       #Fetches all book objects
+    serializer_class = BookSerializer   #use the Bookserializer to serialize the queryset
