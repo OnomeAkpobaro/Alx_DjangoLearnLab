109fa5b0e22cc583b4609b617d460cad4053e667
Onome <Onome@onomes-mbp.home>
2024-11-29 22:27:06 +0000

    Updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from .serializers import AuthorSerializer, BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, BasePermission
 # Create your views here.
 
 #Listview - To retrieve all books
@@ -35,3 +36,13 @@ class BookDeleteView(generics.DestroyAPIView):
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]      #Only authenticated users can delete a book
 
+class IsAuthenticatedOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authenticated users to edit
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        #write permissions are only allowed to authenticated users
+        return request.user
